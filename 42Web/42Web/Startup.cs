b326780f2914564784261a1c0cd259992d1bf919
virtualman18 <virtualman18@gmail.com>
2015-09-30 14:00:16 -0500

    adding the 42 site

---
+++ 42Web/42Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_42Web.Startup))]
+namespace _42Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
